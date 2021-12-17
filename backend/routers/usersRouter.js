b8db83c31befd39a14daeb952ec1a6029d5e5da8
hanranti <hanranti@outlook.com>
2021-12-18 00:37:32 +0200

    Remove congfig and add route without db usage

--- backend/routers/usersRouter.js
+++ backend/routers/usersRouter.js
@@ -11,8 +11,10 @@ router.post('/', async (req, res) => {
   const error = await usersController.createUser(req.body)
   console.log(error)
   error
-    ?res.status(400).send(error)
-    :res.status(200).json({ created: true })
+    ? res.status(400).send(error)
+    : res.status(200).json({ created: true })
 })
+
+router.get('/info', async (req, res) => res.status(200).json('This is an api for users'))
 
 module.exports = router
