224ae8796f4f1fbb312ef2f6ceb2e43941336da5
hanranti <hanranti@outlook.com>
2021-12-18 05:03:57 +0200

    Fix production script

--- data/populate/populateProduction.js
+++ data/populate/populateProduction.js
@@ -2,6 +2,7 @@ const db = require('../../backend/models')
 const sequelize = db.sequelize
 
 const init = async () => {
+  await sequelize.query('CREATE DATABASE IF NOT EXISTS "coin-analyzer-db"')
   await sequelize.query('CREATE TABLE IF NOT EXISTS "user" ("username" TEXT PRIMARY KEY, "passwordHash" TEXT, "name" TEXT, "createdAt" DATE, "updatedAt" DATE)')
 }
 
--- scripts/initializeProduction.sh
+++ scripts/initializeProduction.sh
@@ -1,7 +1,5 @@
 ./scripts/installAndBuild.sh
 
-psql -c "CREATE DATABASE IF NOT EXISTS \"coin-analyzer-db\""
-
 cd data/populate/
 
 node populateProduction.js
