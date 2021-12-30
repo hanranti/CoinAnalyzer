d9aa594b663ab9ed25acda562dc168a08bf68524
hanranti <hanranti@outlook.com>
2021-12-30 16:01:07 +0200

    Fix downward trend

--- frontend/src/utils/coinMath.js
+++ frontend/src/utils/coinMath.js
@@ -1,28 +1,25 @@
 const longestDownwardTrend = (coinData) => {
   let longestStart = coinData[0][0]
-  let longestEnd = longestStart
-  let currentStart = longestStart
-  let previousDate = coinData[0][0]
-  let longestTrend = -1
-  let daysGone = longestTrend
-  let previousValue = coinData[0][1]
+  let longestEnd = coinData[0][0]
+  let currentStart = coinData[0][0]
+  let longestTrendLength = 0
+  let currentTrendLength = 0
+  let previousValue = coinData[0][0]
   coinData.forEach(coin => {
-    if (coin[1] > previousValue && longestTrend < daysGone){
-      longestStart = currentStart
-      longestEnd = previousDate
-      longestTrend = daysGone
-      daysGone = 0
+    if (coin[1] <= previousValue) {
+      if (longestTrendLength <= currentTrendLength) {
+        longestTrendLength = currentTrendLength
+        longestStart = currentStart
+        longestEnd = coin[0]
+      }
+    } else {
       currentStart = coin[0]
-    } else if(coin[1] > previousValue && longestTrend >= daysGone) {
-      daysGone = 0
-      currentStart = coin[0]
-    } else if (coin[1] <= previousValue && longestTrend < daysGone) {
-      daysGone++
+      currentTrendLength = 0
     }
-    previousDate = coin[0]
+    currentTrendLength++
     previousValue = coin[1]
   })
-  return [longestStart, longestEnd, longestTrend]
+  return [longestStart, longestEnd, longestTrendLength]
 }
 
 export default {
